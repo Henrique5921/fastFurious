4be11c79151b9fac4430d40c4cc8865dedc979fd
Henrique <henriquedecastroandrade@gmail.com>
2026-04-15 11:24:34 -0300

    Início - Classe produto concluída e tabela no banco de dados feita

--- src/main/java/br/com/henrique/FastFuriousAPI/domain/model/Produto.java
+++ src/main/java/br/com/henrique/FastFuriousAPI/domain/model/Produto.java
@@ -4,10 +4,89 @@
  */
 package br.com.henrique.FastFuriousAPI.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
 /**
  *
  * @author sesi3dia
  */
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+    private BigDecimal preco;
+    private String categoria;
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String nome, BigDecimal preco, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        return Objects.equals(this.id, other.id);
+    }
     
 }
